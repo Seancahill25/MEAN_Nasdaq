2a5042b7a2574ab5780f72329053aec36e77bc8d
Sean Cahill <seancahill25@gmail.com>
2018-03-11 21:47:46 +0000

    add angular, jquery, and mongodb to app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var routes = require('./api/routes');
 
 
 var app = express();
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/api', routes);
 
 var server = app.listen(app.get("port"), function() {
     var port = server.address().port;
